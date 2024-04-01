ff528f05bcb0ca1c17a9cd0de41202aa0b03a34f
ddwugu <136143092+ddwugu@users.noreply.github.com>
2024-04-01 09:41:34 +0700

    Stream_dataLM.py

--- Stream_dataLM.py
+++ Stream_dataLM.py
@@ -28,6 +28,36 @@ def main():
             st.success(suspect_loct)
         except Exception as e:
             st.error(f"Error predicting location: {e}")
+ # Display the oil loss calculation section
+    st.subheader('Perhitungan Oil Losses')
 
+    def predict_loss(R1, P1, P2, s):
+        R2 = P2 * R1 / P1
+        los = R2 - R1
+        y = los * s
+        return y
+
+    Rate1 = st.text_input('Input rate awal(BBL/Jam)')
+    Pressure1 = st.text_input('Input pressure 1 saat rate awal (PSI)')
+    Pressure2 = st.text_input('Input pressure 2 saat terjadi pressure drop (PSI)')
+    Durasi = st.text_input('Durasi pressure drop (Jam)')
+
+    if st.button('Hitung Losses'):
+        try:
+            R1 = float(Rate1)
+            P1 = float(Pressure1)
+            P2 = float(Pressure2)
+            s = float(Durasi) # Perbaikan pada nama variabel
+            Hitung_Losses = predict_loss(R1, P1, P2, s) # Perbaikan pada argumen
+
+            if Hitung_Losses < 0: # titik nol
+                suspect_loss = f'Terjadi losses sebesar {Hitung_Losses} BBL/Jam '
+            elif Hitung_Losses > 0: # total panjang trunkline
+                suspect_loss = f'Gain sebesar {Hitung_Losses} BBL/Jam'
+            else:
+                suspect_loss = f'Tidak terjadi losses'
+            st.success(suspect_loss)
+        except Exception as e:
+            st.error(f"Error predicting location: {e}")
 if __name__ == "__main__":
     main()
